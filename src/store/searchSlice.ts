84efe2694ec7275e81d520567561794ceda65c2d
smeetsfrank <franksmeets89@gmail.com>
2021-06-02 15:49:36 +0200

    add search slice

---
+++ src/store/searchSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface searchProps {
+  filter: string[];
+}
+
+const initialState: searchProps = {
+  filter: ['album, artist, playlist'],
+};
+
+const searchSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    applyFilter(state, action) {
+      state.filter = [action.payload];
+    },
+  },
+});
+
+export const searchAction = searchSlice.actions;
+export default searchSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import searchReducer from './searchSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    search: searchReducer,
   },
 });
 
