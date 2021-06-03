0297e94e51469dabc1769c77d9cd5db7d2467de7
smeetsfrank <franksmeets89@gmail.com>
2021-06-03 16:22:15 +0200

    add unsplash api

---
+++ src/api/unsplash/index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const { REACT_APP_UNSPLASH_CLIENT_ID: CLIENT_ID } = process.env;
+
+const fetchImage = async () => {
+  const response = await axios.get('https://api.unsplash.com/photos/random', {
+    params: {
+      client_id: CLIENT_ID,
+      query: 'edm',
+      orientation: 'portrait',
+    },
+  });
+  const { data } = response;
+  return data.urls.regular;
+};
+
+export default fetchImage;
