bf330f68a641310ecac4be0d40d8877fb13204a9
smeetsfrank <franksmeets89@gmail.com>
2021-06-07 16:44:10 +0200

    remove unnecessary console.log

--- src/pages/Playlists.tsx
+++ src/pages/Playlists.tsx
@@ -5,20 +5,17 @@ type Props = {
   playlists: any;
 };
 
-const Playlist: React.FC<Props> = ({ playlists }) => {
-  console.log(playlists);
-  return (
-    <>
-      {playlists.map((playlist: any) => (
-        <Card
-          key={playlist.id}
-          name={playlist.name}
-          url={playlist.images[0]?.url}
-          externalUrl={playlist.external_urls?.spotify}
-        />
-      ))}
-    </>
-  );
-};
+const Playlist: React.FC<Props> = ({ playlists }) => (
+  <>
+    {playlists.map((playlist: any) => (
+      <Card
+        key={playlist.id}
+        name={playlist.name}
+        url={playlist.images[0]?.url}
+        externalUrl={playlist.external_urls?.spotify}
+      />
+    ))}
+  </>
+);
 
 export default Playlist;
